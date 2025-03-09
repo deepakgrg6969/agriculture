64ec8c9cc655cdd845487e73e59ac51d53f64b64
gurungnavin <gurungnavin@icloud.com>
2025-03-09 21:57:23 +0900

    responsive design and humburger menu

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop: true,
-    cssMode: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  
-    breakpoints: {
-      1020: {
-          slidesPerView: 2,
-      },
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-    mousewheel: true,
-    keyboard: true,
-  });
-  
-  document.addEventListener('click', function(event) {
-    const sideNav = document.getElementById('sideNav');
-    const hamburger = document.querySelector('.hamburger-icon');
-    
-    if (!sideNav.contains(event.target) && !hamburger.contains(event.target)) {
-        sideNav.classList.remove('active');
-    }
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+    1020: {
+      slidesPerView: 2,
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
 });
 
+let navbar = document.querySelector(".navigation .nav-links");
+let menu = document.querySelector("#menu-btn");
 
-function toggleNav() {
-  document.getElementById("sideNav").classList.toggle("active");
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+
+window.onscroll = () => {
+  navbar.classList.remove("active")
 }
